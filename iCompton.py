42d387ba2757d480cc61a40e73c27de3f52aa197
Zach Davis <zkd@purdue.edu>
2019-06-30 12:07:26 -0400

    small bug correction

--- iCompton.py
+++ iCompton.py
@@ -18,5 +18,6 @@ class iCompton:
         sig_k = np.zeros_like(nu)
         for i in range(len(nu)):
             x = (C.hPlanck * nu[i]) / (self.mq * (C.cLight**2))
-            sig_k[i] = C.sigmaT * (3 / 4) * (((1 + x) / (x**3)) * (((2 * x * (1 + x)) / (1 + 2 * x)) - math.log(1 + 2 * x)) + (1 / 2 * x) * math.log(1 + 2 * x) - ((1 + 3 * x) / ((1 + 2 * x)**2)))
+            sig_k[i] = C.sigmaT * (3 / 4) * ((((1 + x) / (x**3)) * (((2 * x * (1 + x)) / (1 + 2 * x)) - math.log(
+                1 + 2 * x))) + ((1 / (2 * x)) * math.log(1 + 2 * x)) - ((1 + 3 * x) / ((1 + 2 * x)**2)))
         return sig_k
